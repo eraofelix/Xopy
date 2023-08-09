2143819696471c7b877a722114bb4afebe048b55
kun <kylefan15@gmail.com>
2023-08-09 20:12:49 +0800

    update

--- xopy_client.py
+++ xopy_client.py
@@ -4,6 +4,8 @@ import threading
 import datetime
 import firebase_admin
 from firebase_admin import credentials, db
+from pyperclip import PyperclipException
+
 
 class ClipboardMonitor:
     def __init__(self, credential_path, firebase_url):
@@ -13,6 +15,8 @@ class ClipboardMonitor:
         firebase_admin.initialize_app(cred, {
             'databaseURL': firebase_url
         })
+        self.clipboard_ref = db.reference('clipboard')
+        self.clipboard_ref.listen(self.on_clipboard_update)
 
     def start(self):
         self.is_monitoring = True
@@ -23,20 +27,42 @@ class ClipboardMonitor:
         self.is_monitoring = False
         self.thread.join()
 
+    def on_clipboard_update(self, event):
+        clipboard_data = self.clipboard_ref.get()
+        latest_timestamp = max(clipboard_data.keys())
+        latest_clipboard_text = clipboard_data[latest_timestamp]
+        if latest_clipboard_text != self.recent_value and \
+                isinstance(latest_clipboard_text, (str, int, float, bool)):
+            self.recent_value = latest_clipboard_text
+            pyperclip.copy(self.recent_value)  # update the local clipboard
+            print("Clipboard value changed to: " + pyperclip.paste())
+
     def _monitor_clipboard(self):
         while self.is_monitoring:
-            tmp_value = pyperclip.paste()
+            try:
+                tmp_value = pyperclip.paste()
+            except PyperclipException as e:
+                print(f"Failed to paste from clipboard: {e}")
+                continue
+
             if tmp_value != self.recent_value:
                 self.recent_value = tmp_value
                 timestamp = datetime.datetime.now().isoformat()  # get the current time
-                db.reference('clipboard').push({'text': self.recent_value, 'timestamp': timestamp})  # push a new node to the 'clipboard' reference in the Firebase database
+                # push a new node to the 'clipboard' reference in the Firebase database
+                try:
+                    db.reference('clipboard').push(
+                        {'text': self.recent_value, 'timestamp': timestamp})
+                except Exception as e:
+                    print(f"Failed to push to Firebase: {e}")
+                    continue
+
                 print("Clipboard value changed to: " + self.recent_value)
-            time.sleep(0.1)  # pause for a short period to prevent high CPU utilization
+            # pause for a short period to prevent high CPU utilization
+            time.sleep(0.01)
+
 
 # Example usage
 key = "/Users/kun/codes/Xopy/xopy-1f467-firebase-adminsdk-hrzkn-c9b4778f49.json"
-monitor = ClipboardMonitor(key, 'https://xopy-1f467-default-rtdb.firebaseio.com/')
+monitor = ClipboardMonitor(
+    key, 'https://xopy-1f467-default-rtdb.firebaseio.com/')
 monitor.start()
-
-# when you want to stop monitoring
-# monitor.stop()
